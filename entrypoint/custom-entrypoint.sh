b8832ddb8381dcb612a776058e05648ac7670b7e
Francesco Frassinelli <fraph24@gmail.com>
2022-04-13 10:28:43 +0200

    Fix ckan.oauth2.scope

--- entrypoint/custom-entrypoint.sh
+++ entrypoint/custom-entrypoint.sh
@@ -54,7 +54,7 @@ conf_set ckan.oauth2.token_endpoint https://accounts.google.com/o/oauth2/token
 conf_set ckan.oauth2.profile_api_url https://www.googleapis.com/oauth2/v1/userinfo
 conf_set ckan.oauth2.client_id "$OAUTH2_CLIENT_ID"
 conf_set ckan.oauth2.client_secret "$OAUTH2_CLIENT_SECRET"
-conf_set ckan.oauth2.scope "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/plus.me"
+conf_set ckan.oauth2.scope "https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email"
 conf_set ckan.oauth2.profile_api_user_field email
 conf_set ckan.oauth2.profile_api_fullname_field name
 conf_set ckan.oauth2.profile_api_mail_field email

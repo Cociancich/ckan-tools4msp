dd3be81b32183a090d291cf9f4b4bb56ebef5329
Francesco Frassinelli <fraph24@gmail.com>
2022-04-13 12:51:08 +0200

    Fix multivalued custom facets

--- ckanext/ckanext-schemas/ckanext/schemas/plugin.py
+++ ckanext/ckanext-schemas/ckanext/schemas/plugin.py
@@ -1,10 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import json
+
 
 class SchemasPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IFacets, inherit=True)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -19,11 +22,18 @@ class SchemasPlugin(plugins.SingletonPlugin):
     def _facets(self, facets_dict):
         if 'groups' in facets_dict:
             del facets_dict['groups']
-        facets_dict['category'] = toolkit._('Category')
+        facets_dict['vocab_category'] = toolkit._('Category')
         facets_dict['sub_category'] = toolkit._('Sub Category')
         facets_dict['owner'] = toolkit._('Owner')
-        facets_dict['web_services'] = toolkit._('Web services')
+        facets_dict['vocab_web_services'] = toolkit._('Web services')
         return facets_dict
 
     def dataset_facets(self, facets_dict, package_type):
         return self._facets(facets_dict)
+
+    # IPackageController
+
+    def before_index(self, pkg_dict):
+        for field in ('category', 'web_services'):
+            pkg_dict["vocab_"+field] = json.loads(pkg_dict.get(field, '[]'))
+        return pkg_dict

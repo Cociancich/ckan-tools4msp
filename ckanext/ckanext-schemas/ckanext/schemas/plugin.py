504cbcc54894ded6cae8caf2766e23f785b86859
Francesco Frassinelli <fraph24@gmail.com>
2022-04-13 11:56:22 +0200

    Add custom facets

--- ckanext/ckanext-schemas/ckanext/schemas/plugin.py
+++ ckanext/ckanext-schemas/ckanext/schemas/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class SchemasPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
 
@@ -12,3 +13,17 @@ class SchemasPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'schemas')
+
+    # IFacets
+
+    def _facets(self, facets_dict):
+        if 'groups' in facets_dict:
+            del facets_dict['groups']
+        facets_dict['category'] = toolkit._('Category')
+        facets_dict['sub_category'] = toolkit._('Sub Category')
+        facets_dict['owner'] = toolkit._('Owner')
+        facets_dict['web_services'] = toolkit._('Web services')
+        return facets_dict
+
+    def dataset_facets(self, facets_dict, package_type):
+        return self._facets(facets_dict)

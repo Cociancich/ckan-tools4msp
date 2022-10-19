1387e25be1f46bb3d135c6b2e9c092ae63ed3210
Francesco Frassinelli <fraph24@gmail.com>
2022-10-19 20:46:44 +0200

    Topic categories and domain areas with autocomplete

--- ckanext/ckanext-schemas/ckanext/schemas/helpers.py
+++ ckanext/ckanext-schemas/ckanext/schemas/helpers.py
@@ -1,7 +1,16 @@
 import json
+import os.path
 import urllib.request
 
 domainareas_url = "https://api.tools4msp.eu/api/domainareas/?format=json"
+
+file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'presets')
+
+with open(os.path.join(file_dir, 'categories.json')) as categories_file:
+    categories = json.load(categories_file)
+
+def scheming_categories_choices(field):
+    return categories
 
 with urllib.request.urlopen(domainareas_url) as f:
     domainareas = json.load(f)
--- ckanext/ckanext-schemas/ckanext/schemas/views.py
+++ ckanext/ckanext-schemas/ckanext/schemas/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify
 import ckan.plugins.toolkit as toolkit
+from . import helpers
 
 scheming = Blueprint("scheming", __name__)
 
 @scheming.route('/scheming/api/util/<field>/autocomplete')
 def autocomplete(field):
-    results = []
     value = toolkit.request.params.get('incomplete')
     if field == "dataset":
         context = {
@@ -17,8 +17,15 @@ def autocomplete(field):
             "include_private": True,
         }
         found = toolkit.get_action('package_search')(context,  query)
-        for dataset in found['results']:
-            results.append({'Name': dataset['name']})
+        tags = [dataset['name'] for dataset in found['results']]
+    elif field == "category":
+        tags = [tag['value'] for tag in helpers.scheming_categories_choices(field)]
+    elif field == "domain_area":
+        tags = [tag['value'] for tag in helpers.scheming_domainareas_choices(field)]
     else:
         pass
+    results = []
+    for tag in tags:
+        if value.lower() in tag.lower():
+            results.append({'Name': tag})
     return jsonify({"ResultSet": {"Result": results}})

7d67b66181146c8f77740360440a6135a86d598d
Francesco Frassinelli <fraph24@gmail.com>
2023-08-01 22:13:05 +0200

    Support radio as data cluster field_type

--- tools/spreadsheet_to_schema.sh
+++ tools/spreadsheet_to_schema.sh
@@ -21,10 +21,10 @@ create macro labelize(str) as trim(regexp_replace(lcase(str), '[^a-z0-9]+', '-',
 copy (
 select labelize(field_name) as field_name,
        field_name as label,
-       case when field_type = 'multiple_checkbox'
-            then 'multiple_checkbox'
+       case when field_type in ('multiple_checkbox', 'radio')
+            then field_type
        end as preset,
-       case when field_type = 'multiple_checkbox' then
+       case when field_type in ('multiple_checkbox', 'radio') then
        json_group_array(
          json_object(
            'value', labelize(label),
@@ -34,7 +34,7 @@ select labelize(field_name) as field_name,
        else null
        end as choices
   from st_read('$SPREADSHEET', layer='$WORKSHEET')
- where field_type in ('multiple_checkbox', 'text')
+ where field_type in ('multiple_checkbox', 'radio', 'text')
  group by field_name, field_type
 ) to '/dev/stdout' (format json);
 EOF
@@ -49,7 +49,7 @@ load json;
 .mode ascii
 select count(*)
   from st_read('$SPREADSHEET', layer='$WORKSHEET')
- where field_type in ('multiple_checkbox', 'text')
+ where field_type in ('multiple_checkbox', 'radio', 'text')
 EOF
 echo
 
@@ -63,5 +63,5 @@ load json;
 .mode markdown
 select *
   from st_read('$SPREADSHEET', layer='$WORKSHEET')
- where field_type not in ('multiple_checkbox', 'text')
+ where field_type not in ('multiple_checkbox', 'radio', 'text')
 EOF
